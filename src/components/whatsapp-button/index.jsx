cd81720247e4238ab5393c044d253a2f65a2dafd
Abdulrahman Shater <abdulrahmanshatter@gmail.com>
2024-03-10 22:42:49 +0400

    fix

--- src/components/whatsapp-button/index.jsx
+++ src/components/whatsapp-button/index.jsx
@@ -1,10 +1,10 @@
 import { FaWhatsapp } from "react-icons/fa";
-import socialData from "@/data/social";
+// import socialData from "@/data/social";
 
 const WhatsAppButton = () => {
   return (
     <>
-      <a id="whatsappButton" href={`https://wa.me/${socialData.whatsapp}`} >
+      <a id="whatsappButton" href={`https://wa.me/+966920033002`} >
         <FaWhatsapp />
       </a>
     </>
